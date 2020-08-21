2e5675b7035524752f1de03e40c350f5a06a61ea
durvankurp <durvankurp@gmail.com>
2020-08-21 12:04:39 +0530

    test case

--- src/controllers/datacontroller.js
+++ src/controllers/datacontroller.js
@@ -10,13 +10,11 @@ const insertData = async() => {
     const obj = {
       insertdate: new Date(),
     };
-    // const result = await insertRecord(obj);
+    await insertRecord(obj);
     const day = moment(new Date()).format("DD");
-    console.log('isPrime(3): ', isPrime(3));
-    if (isPrime(3)) {
+    if (isPrime(day)) {
       const apidata = await axios.get("https://api.weatherbit.io/v2.0/current?city=Raleigh,NC&key=7b6c6e5dcee14d05ab79fc4f4efef74d");
-      console.log('apidata: ', apidata);
-      return sendResult(false, apidata, null);
+      return sendResult(false, apidata.data, null);
     } else {
       return sendResult(true, null, messages.isNotPrime);
     }
